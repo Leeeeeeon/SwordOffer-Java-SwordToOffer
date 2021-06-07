f42c507b16e10919826906b832d7956f76ac7bd4
Leeeeeeon <45840517+Leeeeeeon@users.noreply.github.com>
2021-06-07 16:10:19 +0800

    Delete t2ReplaceSpaces.java

--- t2ReplaceSpaces.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by z47 on 2021/6/3.
- */
-public class t2ReplaceSpaces {
-        public String replaceSpace (String s) {
-                // write code here
-                return s.toString().replaceAll(" ", "%20");
-                }
-}
